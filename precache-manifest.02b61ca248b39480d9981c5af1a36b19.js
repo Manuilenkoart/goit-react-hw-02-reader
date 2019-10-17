c6fee893a2f7420c90fc5d4eed1e463188afb5a9
Manuilenkoart <manuilenkoart@gmail.com>
2019-10-17 21:38:20 +0300

    Updates

--- precache-manifest.02b61ca248b39480d9981c5af1a36b19.js
+++ precache-manifest.02b61ca248b39480d9981c5af1a36b19.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5d5902b9d5dd8631e19111d6856483ef",
+    "revision": "7d0e29fee592ecbf55d4f94ac9a1abec",
     "url": "/goit-react-hw-02-reader/index.html"
   },
   {
-    "revision": "60666b6c38e9db5f2f49",
+    "revision": "26564c0707786514da2b",
     "url": "/goit-react-hw-02-reader/static/css/main.e4f9a740.chunk.css"
   },
   {
-    "revision": "44581860dd8ef138b4e4",
-    "url": "/goit-react-hw-02-reader/static/js/2.2ff0548d.chunk.js"
+    "revision": "bfdbabfae7fc335d7a36",
+    "url": "/goit-react-hw-02-reader/static/js/2.cc001f8a.chunk.js"
   },
   {
-    "revision": "60666b6c38e9db5f2f49",
-    "url": "/goit-react-hw-02-reader/static/js/main.671139f1.chunk.js"
+    "revision": "26564c0707786514da2b",
+    "url": "/goit-react-hw-02-reader/static/js/main.68853898.chunk.js"
   },
   {
     "revision": "0da2320c246f0dcbed6e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-02-reader/precache-manifest.7fab5a81099b0af00c7f55e59bdebbce.js"
+  "/goit-react-hw-02-reader/precache-manifest.02b61ca248b39480d9981c5af1a36b19.js"
 );
 
 self.addEventListener('message', (event) => {
