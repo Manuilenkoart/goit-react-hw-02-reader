4f5e9af720bad9183c91153733298352d8d32456
Manuilenkoart <manuilenkoart@gmail.com>
2019-10-17 12:13:49 +0300

    Updates

---
+++ precache-manifest.7fab5a81099b0af00c7f55e59bdebbce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d5902b9d5dd8631e19111d6856483ef",
+    "url": "/goit-react-hw-02-reader/index.html"
+  },
+  {
+    "revision": "60666b6c38e9db5f2f49",
+    "url": "/goit-react-hw-02-reader/static/css/main.e4f9a740.chunk.css"
+  },
+  {
+    "revision": "44581860dd8ef138b4e4",
+    "url": "/goit-react-hw-02-reader/static/js/2.2ff0548d.chunk.js"
+  },
+  {
+    "revision": "60666b6c38e9db5f2f49",
+    "url": "/goit-react-hw-02-reader/static/js/main.671139f1.chunk.js"
+  },
+  {
+    "revision": "0da2320c246f0dcbed6e",
+    "url": "/goit-react-hw-02-reader/static/js/runtime-main.21ebc90b.js"
+  }
+]);
